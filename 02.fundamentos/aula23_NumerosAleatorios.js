df7ab0545207f8ad3d1797d294a03c039d9612e7
Hans <hans28h@gmail.com>
2026-03-07 01:00:38 -0300

    Implement random selection from a list
    Add functionality to select a random value from a list.

--- 02.fundamentos/aula23_NumerosAleatorios.js
+++ 02.fundamentos/aula23_NumerosAleatorios.js
@@ -1,2 +1,5 @@
 //         (arredonda um número para baixo(cria números aleatórios, mas com varias casas decimais))
 console.log(Math.floor(Math.random() * 10)); //cria um número aleatório de 0 a 9;
+
+let lista = ['ab', 'bc', 'cd'];
+console.log(lista[Math.floor(Math.random() * lista.length)]); //seleciona um valor de uma lista
